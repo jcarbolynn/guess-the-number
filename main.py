7743d47c8f8574ef7d01b448516dc9d59f0cd05d
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-08-03 13:39:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,92 @@
-from art import logo
+from game_data import data
+import random
+from art import logo, vs
+from replit import clear
 
-#global constant for levels
-EASY_LEVEL = 10
-HARD_LEVEL = 5
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
 
-turns = 0
-def checkAnswer(guess, answer, turns):
-  '''checks answer against guess, returns number of turns remaining'''
-  if guess > answer:
-    print("Too high")
-    return turns -1
-  elif guess < answer:
-    print("Too low")
-    return turns -1
+def format_data(account):
+  """Format account into printable format: name, description and country"""
+  name = account["name"]
+  description = account["description"]
+  country = account["country"]
+  # print(f'{name}: {account["follower_count"]}')
+  return f"{name}, a {description}, from {country}"
+
+def check_answer(guess, a_followers, b_followers):
+  """Checks followers against user's guess 
+  and returns True if they got it right.
+  Or False if they got it wrong.""" 
+  if a_followers > b_followers:
+    return guess == "a"
   else:
-    print(f"You got it! The answer was {answer}.")
+    return guess == "b"
 
-def setDifficulty():
-  difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-  if difficulty == "easy":
-    return EASY_LEVEL
-  else:
-    return HARD_LEVEL
 
-print(logo)
-
-#put game functionality inside own function
 def game():
-  print("Welcome to the Number Guessing Game!")
-  print("I'm thinking of a number between 1 and 100.")
-  import random
-  answer = random.randint(1, 100)
-  print(f"Psst, the correct answer is {answer}")
-  
-  #function to set difficulty
-  turns = setDifficulty()
-  
-  
-  #user guess number
-  guess = 0
-  while guess != answer:
-    print(f"You have {turns} attempts remaining to guess the number.")
-    guess = int(input("Guess a number: "))
-    #because checkAnswer() returns number of turns, save it to the turns variable
-    #doesnt need to be global because not in new function from where turns declared
-    turns = checkAnswer(guess, answer, turns)
-    if turns == 0:
-      print("You've run out of guesses you lose.")
-      return
-    elif guess != answer:
-      print("Guess again.")
+  print(logo)
+  score = 0
+  game_should_continue = True
+  account_a = get_random_account()
+  account_b = get_random_account()
+
+  while game_should_continue:
+    account_a = account_b
+    account_b = get_random_account()
+
+    while account_a == account_b:
+      account_b = get_random_account()
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
+    
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    clear()
+    print(logo)
+    if is_correct:
+      score += 1
+      print(f"You're right! Current score: {score}.")
+    else:
+      game_should_continue = False
+      print(f"Sorry, that's wrong. Final score: {score}")
 
 game()
 
-#function to check user guess against number
+'''
 
-#track number of turns and reduce by 1 if wrong guess
+FAQ: Why does choice B always become choice A in every round, even when A had more followers? 
 
-# repeat guessing if wrong guess and still have turns left
+Suppose you just started the game and you are comparing the followers of A - Instagram (364k) to B - Selena Gomez (174k). Instagram has more followers, so choice A is correct. However, the subsequent comparison should be between Selena Gomez (the new A) and someone else. The reason is that everything in our list has fewer followers than Instagram. If we were to keep Instagram as part of the comparison (as choice A) then Instagram would stay there for the rest of the game. This would be quite boring. By swapping choice B for A each round, we avoid a situation where the number of followers of choice A keeps going up over the course of the game. Hope that makes sense :-)
+
+'''
+
+
+
+# Generate a random account from the game data.
+
+# Format account data into printable format.
+
+# Ask user for a guess.
+
+# Check if user is correct.
+## Get follower count.
+## If Statement
+
+# Feedback.
+
+# Score Keeping.
+
+# Make game repeatable.
+
+# Make B become the next A.
+
+# Add art.
+
+# Clear screen between rounds.
